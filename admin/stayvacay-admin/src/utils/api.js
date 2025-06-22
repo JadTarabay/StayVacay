385377f21a9af26b26af9e578a404f5edcc5f818
JAD TARABAY <jhtarabay02@ndu.edu.lb>
2025-06-22 16:21:32 +0300

    frontend api update

--- admin/stayvacay-admin/src/utils/api.js
+++ admin/stayvacay-admin/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // update with your backend URL
+  baseURL: import.meta.env.REACT_APP_API_URL,
 });
 
 api.interceptors.request.use(config => {
