d3e131d4537d1da1f67b61d2d4fa0cd89643e503
JAD TARABAY <jhtarabay02@ndu.edu.lb>
2025-09-30 17:07:20 +0300

    Bring nack AWS S3

--- server/stayvacay-server/server.js
+++ server/stayvacay-server/server.js
@@ -12,6 +12,8 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
+
 dotenv.config();
 const app = express();
 
